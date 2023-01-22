d7f2d8a4f90c2a8fdd1e94290b9529b0d0fcc5c3
piatea <takehiro.matsumoto.github@gmail.com>
2023-01-22 17:11:13 +0900

    ♻️ add the router to setting and main page

--- src/components/initial/InitialButtons.jsx
+++ src/components/initial/InitialButtons.jsx
@@ -1,7 +1,18 @@
 import styles from '@/src/styles/Home.module.css'
 import { Stack, Button } from '@mui/material'
+import { useRouter } from 'next/router'
 
 const InitialButtons = () => {
+  const router = useRouter()
+
+  const handleStart = () => {
+    router.push('/MainPage')
+  }
+
+  const handleSetting = () => {
+    router.push('/SettingPage')
+  }
+
   return (
     <Stack 
       spacing={2}
@@ -10,12 +21,14 @@ const InitialButtons = () => {
       <Button
         className={styles.initial_screen_button}
         variant="outlined"
+        onClick={handleStart}
       >
         はじめる
       </Button>
       <Button
         className={styles.initial_screen_button}
         variant="outlined"
+        onClick={handleSetting}
       >
         オプション
       </Button>
